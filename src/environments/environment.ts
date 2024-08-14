d479c895c35745693f74447392057d3ebe48de10
samiksha-sherekar <samikshasherekar01@gmail.com>
2024-08-14 12:25:19 +0530

    project Deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    baseurl:"https://ca6eee7046e3a72bc148.free.beeceptor.com/api",
+    baseurl:"https://ca96146811bf7e2d9104.free.beeceptor.com/api",
     // baseurl: "https://cada266ab3f06eaceb30.free.beeceptor.com/api"
 };
